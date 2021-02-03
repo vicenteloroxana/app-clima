dd48c0894af55305734b7f309a1a3e7acb9370b9
roxana abigail vicentelo <abigailvicentelo@gmail.com>
2021-02-03 00:11:39 -0300

    construyendo  archivo de distribucion

--- src/app/index.js
+++ src/app/index.js
@@ -1,35 +1,32 @@
-require('./index.css');
-const { Store } = require('./store');
+const { Store } = require('./Store');
 const { Weather } = require('./Weather');
 const { UI } = require('./UI');
 
 //index clase controladora de las demas clases 
-const store=new Store();
-//obtengo la city y country code por defecto al iniciar la app
+const storage = new Store();
 const {city, countryCode}= storage.getLocationData();
+const weather = new Weather(city, countryCode);
+//obtengo la city y country code por defecto al iniciar la app
+
+//al iniciar la app sera con valores por defecto 
+
 const ui = new UI();
 //console.log('objeto',ui);
-//al iniciar la app sera con valores por defecto 
-const weather=new Weather(city, countryCode);
 //console.log(weather);
-
+require('./index.css');
 document.addEventListener('DOMContentLoaded',fetchWeather);
 
-async function fetchWeather(){
-    const data=await weather.getWeather();
-    //console.log(data);
+async function fetchWeather() {
+    const data = await weather.getWeather()
     ui.render(data);
-    //console.log('ui ',ui.render(data));
-    
 }
 document.getElementById('w-change-btn').addEventListener('click', (e) => {
     const city = document.getElementById('city').value;
-    const countryCode = document.getElementById('countryCode').value;
-    //console.log(city,countryCode); a mi mismo obj o instancia le cambio las propiedades
-    weather.changeLocation(city,countryCode);
-    store.setLocationData(city,countryCode);
-    e.preventDefault();
-    // weather.changeLocation(city, countryCode);
-    // storage.setLocationData(city, countryCode);
-    fetchWeather();  
+    const countryCode = document.getElementById('countryCode').value
+    weather.changeLocation(city, countryCode);
+    storage.setLocationData(city, countryCode);
+    fetchWeather();
+  
 });
+  
+document.addEventListener('DOMContentLoaded', fetchWeather);
